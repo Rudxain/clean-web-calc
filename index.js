02e6eccbf429b742090dded037971be77df677e1
Rudxain <76864299+Rudxain@users.noreply.github.com>
2025-11-19 14:33:34 -0400

    handle keyboard; do event delegation; etc...

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
-"use strict";
-let calc = "";
+//@ts-check
+"use strict"
+const root = /**@type {HTMLDivElement}*/(document.getElementById("root"))
+const display = /**@type {HTMLParagraphElement}*/(document.getElementById("display"))
 
-function updateDisplay(text) {
-	document.getElementById("display").innerHTML = text;
-}
+// isolate mutable state
+{
+	const op_or_group = new Set("()<>+-*/.")
 
-function input(value) {
-	if (calc.length > 16) return;
-	calc += value;
-	updateDisplay(calc);
-}
+	/**
+	Expression. Mostly mirrors display
+	*/
+	let xpr = ""
 
-function calculate() {
-	try {
-		calc = eval(calc).toString();
-		if (calc == "NaN") {
-			calc = "";
-			updateDisplay("undefined");
-		} else 
-			updateDisplay(calc);
-	} catch (err) {
-		calc = "";
-		updateDisplay("error");
+	/**
+	@param {string} b
+	*/
+	const handle_any_button_or_key = b => {
+		if ((b >= "0" && b <= "9") || op_or_group.has(b)) {
+			if (xpr.length > 16) return;
+			display.textContent = xpr += b
+			return
+		}
+		if (b == "c" || b == "C") {
+			display.textContent = xpr = ""
+			return
+		}
+		if (b == "=")
+			try {
+				// indirect is safer and faster
+				xpr = (eval)?.(xpr).toString();
+				if (xpr == "NaN") {
+					xpr = "";
+					display.textContent = "undefined";
+				} else
+					display.textContent = xpr;
+			} catch (err) {
+				xpr = "";
+				// some error messages can be too long,
+				// or might have misleading info in this context,
+				// so show generic msg
+				display.textContent = "error!";
+			}
+		// ignore invalid input
 	}
-}
 
-function displayClear() {
-	calc = "";
-	updateDisplay("");
+	root.addEventListener("click", e => {
+		const el = /**@type {HTMLDivElement|HTMLParagraphElement|HTMLButtonElement|HTMLBRElement}*/(
+			e.target
+		)
+		if (el.tagName == "P") return
+		handle_any_button_or_key(el.textContent)
+	})
+
+	root.addEventListener("keydown", e => {
+		handle_any_button_or_key(e.key)
+	})
 }
